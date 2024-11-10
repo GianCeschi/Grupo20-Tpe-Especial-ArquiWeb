2e574c9c03d259fc460bad414b60b0a2129dc98d
pablo-andersen <pandersen@alumnos.exa.unicen.edu.ar>
2024-11-10 18:39:44 -0300

    Se completó la entidad Parada

--- microMonopatin/src/main/java/org/example/micromonopatin/entity/Parada.java
+++ microMonopatin/src/main/java/org/example/micromonopatin/entity/Parada.java
@@ -1,4 +1,49 @@
 package org.example.micromonopatin.entity;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
+
+@Document(collection = "paradas")
 public class Parada {
+
+    @Id
+    private String idParada;
+
+    private double longitud;
+    private double latitud;
+    private int capacidadMaxima;
+
+    // Getters y setters
+    public String getIdParada() {
+        return idParada;
+    }
+
+    public void setIdParada(String idParada) {
+        this.idParada = idParada;
+    }
+
+    public double getLongitud() {
+        return longitud;
+    }
+
+    public void setLongitud(double longitud) {
+        this.longitud = longitud;
+    }
+
+    public double getLatitud() {
+        return latitud;
+    }
+
+    public void setLatitud(double latitud) {
+        this.latitud = latitud;
+    }
+
+    public int getCapacidadMaxima() {
+        return capacidadMaxima;
+    }
+
+    public void setCapacidadMaxima(int capacidadMaxima) {
+        this.capacidadMaxima = capacidadMaxima;
+    }
 }
