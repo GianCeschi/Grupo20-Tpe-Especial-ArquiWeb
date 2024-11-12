de164ceff219814fea9a41fee1e7f241b3f14c01
Gianluca Ceschinelli <ceschinelligianluca@gmail.com>
2024-11-11 21:53:08 -0300

    Creamos las 3 bases de datos, implementamos CRUD y metodos de consignas de reportes de Monopatin.

--- microUsuario/src/main/java/org/example/microusuario/repository/UsuarioRepository.java
+++ microUsuario/src/main/java/org/example/microusuario/repository/UsuarioRepository.java
@@ -1,7 +1,8 @@
 package org.example.microusuario.repository;
 
+import org.example.microusuario.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UsuarioRepository extends JpaRepository {
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
 }
--- microViaje/src/main/java/org/example/microviaje/repository/ViajeRepository.java
+++ microViaje/src/main/java/org/example/microviaje/repository/ViajeRepository.java
@@ -1,6 +1,7 @@
 package org.example.microviaje.repository;
 
+import org.example.microviaje.entity.Viaje;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ViajeRepository extends JpaRepository {
+public interface ViajeRepository extends JpaRepository <Viaje, Long> {
 }
