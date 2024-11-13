8bd31870f1287df66bc4bd151055ba89d12d24be
pablo-andersen <pandersen@alumnos.exa.unicen.edu.ar>
2024-11-12 22:02:27 -0300

    se realiza una ultima correccion en repositorio y servicio de usuario x2

--- microUsuario/src/main/java/org/example/microusuario/repository/UsuarioRepository.java
+++ microUsuario/src/main/java/org/example/microusuario/repository/UsuarioRepository.java
@@ -3,7 +3,6 @@ package org.example.microusuario.repository;
 import org.example.microusuario.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
