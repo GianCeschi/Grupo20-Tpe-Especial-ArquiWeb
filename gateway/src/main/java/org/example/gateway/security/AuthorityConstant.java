f77e10b8ef31a69cddbe2256a52e0c367799620b
pablo-andersen <pandersen@alumnos.exa.unicen.edu.ar>
2024-11-18 15:36:53 -0300

    Se crea estructura de JWT para segurización, todavía falta adaptarla al TPE

---
+++ gateway/src/main/java/org/example/gateway/security/AuthorityConstant.java
@@ -0,0 +1,7 @@
+package org.example.gateway.security;
+
+public class AuthorityConstant {
+    public static final String _ADMIN = "ADMIN";
+    public static final String _PROFESOR = "PROFESOR";
+    public static final String _ALUMNO = "ALUMNO";
+}
