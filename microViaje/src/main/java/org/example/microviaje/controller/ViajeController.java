29ccea3c33bab04aaa5c8877667ded78afba69a0
pablo-andersen <pandersen@alumnos.exa.unicen.edu.ar>
2024-11-20 16:47:57 -0300

    anotaciones de documentación Swagger en servicio viajecontroller

--- microViaje/src/main/java/org/example/microviaje/controller/ViajeController.java
+++ microViaje/src/main/java/org/example/microviaje/controller/ViajeController.java
@@ -69,8 +69,8 @@ public class ViajeController {
     @Operation(
             summary = "Obtener detalle de un viaje.",
             description = "Un usuario autenticado con rol ADMIN, puede obtener la información detallada de un viaje de la aplicacion, a partir de un idViaje.",
-            operationId = "getOneMonopatin",
-            tags = {"Monopatin", "Reporte"},
+            operationId = "getOneViaje",
+            tags = {"Viaje", "Reporte"},
             responses = {
                     @ApiResponse(responseCode = "200", description = "Solicitud exitosa"),
                     @ApiResponse(responseCode = "400", description = "Solicitud incorrecta")
@@ -81,18 +81,48 @@ public class ViajeController {
         return viajeServicio.getById(id);
     }
 
+    @Operation(
+            summary = "Eliminar un viaje",
+            description = "Un usuario autenticado con rol ADMIN, puede eliminar un viaje de la aplicacion indicando el idViaje.",
+            operationId = "deleteViaje",
+            tags = {"Viaje", "Eliminar"},
+            responses = {
+                    @ApiResponse(responseCode = "204", description = "Viaje eliminado con éxito"),
+                    @ApiResponse(responseCode = "400", description = "Solicitud incorrecta")
+            }
+    )
     @DeleteMapping("/{id}")
     public ResponseEntity<String> delete(@PathVariable Long id){return viajeServicio.delete(id);}
 
+
+    @Operation(
+            summary = "Obtener reporte  de monopatines con mas de x viajes en un determinado año.",
+            description = "Un usuario autenticado con rol ADMIN, puede obtener un reporte de monopatines con mas de una cantidad dada de viajes durante un año dado.",
+            operationId = "getReportePorViajesEnUnAnio",
+            tags = {"Viaje", "Monopatin", "Reporte"},
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Solicitud exitosa"),
+                    @ApiResponse(responseCode = "400", description = "Solicitud incorrecta")
+            }
+    )
     @GetMapping("/viajesPorMonopatin/{anio}/{cant}")
     public Iterable<ReporteViajeMonopatinDTO> getViajesPorMonopatin(@PathVariable int anio, @PathVariable int cant) throws Exception{
         return viajeServicio.getViajesPorMonopatin(anio, cant);
     }
 
+    @Operation(
+            summary = "Obtener reporte  de facturacion.",
+            description = "Un usuario autenticado con rol ADMIN, puede obtener un reporte del total facturado en un rango de meses de cierto año.",
+            operationId = "getReportePorViajesEnUnAnio",
+            tags = {"Facturacion", "Reporte"},
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Solicitud exitosa"),
+                    @ApiResponse(responseCode = "400", description = "Solicitud incorrecta")
+            }
+    )
     @GetMapping("/facturacion/{mes1}/{anio1}/{mes2}/{anio2}")
     public Double getTotalFacturacion(@PathVariable int mes1, @PathVariable int anio1,
                                       @PathVariable int mes2, @PathVariable int anio2) throws Exception {
         return viajeServicio.getTotalFacturacion(mes1,anio1,mes2,anio2);
     }
-
 }
