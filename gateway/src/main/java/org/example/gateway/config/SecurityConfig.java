221cb082a0b6b97a9359e6c6da68013921f91d7c
pablo-andersen <pandersen@alumnos.exa.unicen.edu.ar>
2024-11-20 00:45:42 -0300

    Pequeña corrección de segurización

--- gateway/src/main/java/org/example/gateway/config/SecurityConfig.java
+++ gateway/src/main/java/org/example/gateway/config/SecurityConfig.java
@@ -44,7 +44,7 @@ public class SecurityConfig {
             .authorizeHttpRequests(authz -> authz
                     .requestMatchers(HttpMethod.POST, "/api/authenticate").permitAll()
                     .requestMatchers(HttpMethod.POST, "/api/users").permitAll()
-                    .requestMatchers("/api/monopatines/mantenimiento").hasAuthority(AuthorityConstant._MANTENIMIENTO)
+                    .requestMatchers("/api/monopatines/mantenimiento/**").hasAuthority(AuthorityConstant._MANTENIMIENTO)
                     .requestMatchers(HttpMethod.PATCH, "/api/monopatines").hasAuthority(AuthorityConstant._USUARIO)
                     .requestMatchers(HttpMethod.GET, "/api/monopatines/cercanos").hasAuthority(AuthorityConstant._USUARIO)
                     .requestMatchers("/api/monopatines/**").hasAuthority( AuthorityConstant._ADMIN )
