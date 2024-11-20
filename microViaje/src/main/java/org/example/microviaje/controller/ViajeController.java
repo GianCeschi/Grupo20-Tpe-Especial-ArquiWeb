c7ece077fd815e9fd3494de4f42a431a4c3652e8
pablo-andersen <pandersen@alumnos.exa.unicen.edu.ar>
2024-11-20 16:13:40 -0300

    anotaciones de documentación Swagger en servicio viajecontroller

--- microViaje/src/main/java/org/example/microviaje/controller/ViajeController.java
+++ microViaje/src/main/java/org/example/microviaje/controller/ViajeController.java
@@ -1,5 +1,7 @@
 package org.example.microviaje.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.example.microviaje.dto.ReporteViajeMonopatinDTO;
 import org.example.microviaje.dto.RequestViajeDTO;
 import org.example.microviaje.dto.ViajeDTO;
@@ -19,24 +21,61 @@ public class ViajeController {
     public ViajeController(ViajeServicio viajeServicio) {
         this.viajeServicio = viajeServicio;
     }
-
+    @Operation(
+            summary = "Registrar el inicio de un viaje.",
+            description = "Un usuario autenticado con rol USUARIO, comienza a utilizar un monopatín dando inicio a un viaje, el cual se crea desde este endpoint.",
+            operationId = "saveViaje",
+            tags = {"Viaje", "Crear"},
+            responses = {
+                    @ApiResponse(responseCode = "201", description = "Viaje creado correctamente")
+            }
+    )
     @PostMapping("")
     public ResponseEntity<ViajeDTO> save(@RequestBody RequestViajeDTO request) {
         final var result =  this.viajeServicio.save(request);
         return ResponseEntity.ok().body(result);
     }
 
+    @Operation(
+            summary = "Modificar datos de un viaje.",
+            description = "Un usuario autenticado con rol USUARIO, actualizar la información de su viaje.",
+            operationId = "updateViaje",
+            tags = {"Viaje"},
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Solicitud exitosa")
+            }
+    )
     @PatchMapping("/{id}")
     public ResponseEntity<ViajeDTO> update(@PathVariable Long id, @RequestBody RequestViajeDTO nuevoViaje) throws Exception {
         var resultado = viajeServicio.update(id, nuevoViaje);
         return ResponseEntity.ok().body(resultado);
     }
 
+    @Operation(
+            summary = "Obtener listado de viajes.",
+            description = "Un usuario autenticado con rol ADMIN, puede obtener un listado de todos los viajes generados en la aplicacion.",
+            operationId = "getAllViajes",
+            tags = {"Viaje", "Reporte"},
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Solicitud exitosa"),
+                    @ApiResponse(responseCode = "400", description = "Solicitud incorrecta")
+            }
+    )
     @GetMapping("")
     public Iterable<Viaje> getAll() {
         return viajeServicio.getAll();
     }
 
+    @Operation(
+            summary = "Obtener detalle de un viaje.",
+            description = "Un usuario autenticado con rol ADMIN, puede obtener la información detallada de un viaje de la aplicacion, a partir de un idViaje.",
+            operationId = "getOneMonopatin",
+            tags = {"Monopatin", "Reporte"},
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Solicitud exitosa"),
+                    @ApiResponse(responseCode = "400", description = "Solicitud incorrecta")
+            }
+    )
     @GetMapping("/{id}")
     public ResponseEntity<Viaje> get(@PathVariable Long id) {
         return viajeServicio.getById(id);
